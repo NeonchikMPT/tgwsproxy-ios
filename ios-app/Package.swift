435348f45259e0d99ae023a5a3d421387d620e44
NeonchikMPT <115140656+NeonchikMPT@users.noreply.github.com>
2026-04-15 04:25:18 +0300

    Update Package.swift
    Fix iOS version to 17

--- ios-app/Package.swift
+++ ios-app/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "TgWsProxy",
     platforms: [
-        .iOS(.v16_2)
+        .iOS(.v17)
     ],
     products: [
         .executable(name: "TgWsProxy", targets: ["TgWsProxy"]),
